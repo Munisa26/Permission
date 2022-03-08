0b25cc9e5a576fbb623f0910f94f66956b6e011e
Munisa26 <98171600+Munisa26@users.noreply.github.com>
2022-03-08 12:14:50 +0500

    Yangi PermissionManagment

---
+++ PermissionManagement/PermissionManagement/Constants/Roles.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PermissionManagement.Constants
+{
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Basic
+    }
+}
---
+++ PermissionManagement/PermissionManagement/Models/UserRolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PermissionManagement.Models
+{
+    public class UserRolesViewModel
+    {
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+    public class ManageUserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public IList<UserRolesViewModel> UserRoles { get; set; }
+    }
+}
